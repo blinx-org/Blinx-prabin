9b0bcfe85849ba8132268f7d0493ffd94c8298a4
mraju2 <search4gyan@gmail.com>
2015-12-09 17:33:20 +0530

    New functions added
    volunteer_requests  and get_all_requests functions added. volunteer_requests function will retrieve request 's of volunteer accepted and completed.
    get_all_requests  will retrieve all the requests data from database. This is used for moderator search functionality.

--- libs/search.php
+++ libs/search.php
@@ -2,6 +2,142 @@
 
 function get_app_by_id() {
     return run_query();
+	//echo run_query();
+}
+function get_volunteer_requests()
+{
+	echo volunteer_requests();
+}
+function get_all_requests()
+{
+	$value=all_requests();
+	echo json_encode($value);
+}
+
+function all_requests()
+{
+	 // including db connection details into search backend
+    include_once 'dbconnection.php';
+
+   
+    $where='';
+    $conn = mysqli_connect($host, $user, $pass, $database) or die("Error " . mysqli_error($link));
+    $query = "SELECT  h.Id, h.Description, u.first_name, u.last_name, u.mobile_number, u.gender, u.user_id, req.Id as reqID, req.Message,req.Address,req.Location,req.Createddate,req.Requesteddate,req.latitude,req.longitude,req.duration FROM  t_help_request req, m_user u , f_help h where req.userId = u.user_id and h.Id = req.helpId";
+
+	  if (isset($_GET["distance_range"]) && $_GET["distance_range"] != '') {
+		$distanceFilter = isset($_GET["distance_range"]) ? $_GET["distance_range"] : 4;
+		 $long = $_GET["long"];
+		$lat = $_GET["lat"];
+		$longitude_low = $long - ($distanceFilter / 100);
+		$longitude_high = $long + ($distanceFilter / 100);
+		$latitude_low = $lat - ($distanceFilter / 100);
+		$latitude_high = $lat + ($distanceFilter / 100);
+		$where = " and req.longitude between " . $longitude_low . " and " . $longitude_high . " and req.latitude between " . $latitude_low . " and " . $latitude_high;
+	  }
+	// Filtering logic
+    if (isset($_GET["service_type"]) && $_GET["service_type"] != '') {
+        $where = $where . " and req.helpId in ( " . $_GET["service_type"] . ") ";
+    }
+
+    if (isset($_GET["date_range"]) && $_GET["date_range"] != '') {
+        $help_dates = explode(",", $_GET["date_range"]);
+        $multipleDatesPresent = false;
+        $datequery = "";
+        foreach ($help_dates as $filter_date) {
+            if ($multipleDatesPresent) {
+                $datequery = $datequery . " OR ";
+            } else {
+                $where = $where . " and ( ";
+                $multipleDatesPresent = true;
+            }
+            switch ($filter_date) {
+                case 1 :
+                    $current_date = date('Y-m-d', time());
+                    $datequery = $datequery . " (req.Requesteddate  >= STR_TO_DATE('$current_date', '%Y-%m-%d')
+							and req.Requesteddate < DATE_ADD(STR_TO_DATE('$current_date', '%Y-%m-%d'), INTERVAL 1 DAY ))";
+
+                    break;
+                case 2 :
+                    $current_date = date('Y-m-d', time());
+                    $current_date = new DateTime($current_date);
+                    $current_date->modify('+1 day');
+                    $tommorow_date = $current_date->format('Y-m-d');
+                    $datequery = $datequery . " (req.Requesteddate  >= STR_TO_DATE('$tommorow_date', '%Y-%m-%d')
+							and req.Requesteddate < DATE_ADD(STR_TO_DATE('$tommorow_date', '%Y-%m-%d'), INTERVAL 1 DAY ))";
+                    break;
+                case 3 :
+                    $current_date = date('Y-m-d', time());
+                    $current_date = new DateTime($current_date);
+                    $current_date->modify('+2 day');
+                    $tommorow_date = $current_date->format('Y-m-d');
+                    $datequery = $datequery . " (req.Requesteddate  > STR_TO_DATE('$tommorow_date', '%Y-%m-%d'))";
+
+                    break;
+            }
+        }
+        $where = $where . $datequery . ")";
+    } else {
+        $current_date = date('Y-m-d', time());
+        $where = $where . " and  req.Requesteddate  >= STR_TO_DATE('$current_date', '%Y-%m-%d')";
+    }
+    //
+  //  if (isset($_GET["duration_low"]) && $_GET["duration_low"] != '') {
+  //      $where = $where . " and req.duration >= " . $_GET["duration_low"];
+  //  }
+    if (isset($_GET["duration_range"]) && $_GET["duration_range"] != '') {
+        $where = $where . " and req.duration <= " . $_GET["duration_range"];
+    }
+    if (isset($_GET["mode"]) && $_GET["mode"] != '') {
+        $where = $where . " and req.mode in ( " . $_GET["help_mode"] . ") ";
+    }
+    if (isset($_GET["status"]) && $_GET["status"] != '') {
+        $where = $where . " and req.status in ( " . $_GET["status"] . ") ";
+    }
+    //sort order comes here
+    if (isset($_GET["order"]) && isset($_GET["order"]) != '') {
+        if (isset($_GET["order"]) == 'asc') {
+            $order = " order by req.requesteddate asc";
+        }
+        $order = " order by req.requesteddate desc";
+    } else {
+        $order = " order by req.requesteddate desc";
+    }
+
+    $sql_final = $query . $where . $order;
+
+    $result = mysqli_query($conn, $sql_final);
+    if (!$result) {
+        echo "Could not successfully run query ($sql_final) from DB: " . mysql_error();
+        exit;
+    }
+    $data['requests'] = array();
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        array_push($data['requests'], $row);
+    }
+    //
+    mysqli_close($conn);
+    return $data;
+	//return $sql_final;
+}
+
+
+
+function volunteer_requests() 
+{
+	 // including db connection details into search backend
+    include_once 'dbconnection.php';
+	$vid = $_GET["vid"];
+	 $conn = mysqli_connect($host, $user, $pass, $database) or die("Error " . mysqli_error($link));
+    $query = "SELECT  h.Id, h.Description, u.first_name, u.last_name, u.mobile_number, u.gender," .
+	" u.user_id, req.Id, req.Message,req.Address,req.Location,req.Createddate,req.Requesteddate,req.latitude,".
+	"req.longitude,req.duration,req.Accepteddate,req.status FROM  t_help_request req, m_user u , f_help h ".
+	"where req.userId = u.user_id and h.Id = req.helpId and req.VolunteerId=".$vid."";
+	$result = mysqli_query($conn, $query);
+	$data['requests'] = array();
+	 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        array_push($data['requests'], $row);
+    }
+	echo json_encode($data['requests']);
 }
 
 /**
@@ -15,17 +151,21 @@ function run_query() {
 
     $long = $_GET["long"];
     $lat = $_GET["lat"];
-    $distanceFilter = isset($_GET["distance_range"]) ? $_GET["distance_range"] : 4;
-
+    
     $conn = mysqli_connect($host, $user, $pass, $database) or die("Error " . mysqli_error($link));
-    $query = "SELECT  h.Id, h.Description, u.first_name, u.last_name, u.mobile_number, u.gender, u.user_id, req.Id, req.Message,req.Address,req.Location,req.Createddate,req.Requesteddate,req.latitude,req.longitude,req.duration FROM  t_help_request req, m_user u , f_help h where req.userId = u.user_id and h.Id = req.helpId";
+    $query = "SELECT  h.Id, h.Description, u.first_name, u.last_name, u.mobile_number, u.gender, u.user_id, req.Id as reqID, req.Message,req.Address,req.Location,req.Createddate,req.Requesteddate,req.latitude,req.longitude,req.duration FROM  t_help_request req, m_user u , f_help h where req.userId = u.user_id and h.Id = req.helpId";
 
 
-    $longitude_low = $long - ($distanceFilter / 100);
-    $longitude_high = $long + ($distanceFilter / 100);
-    $latitude_low = $lat - ($distanceFilter / 100);
-    $latitude_high = $lat + ($distanceFilter / 100);
-    $where = " and req.longitude between " . $longitude_low . " and " . $longitude_high . " and req.latitude between " . $latitude_low . " and " . $latitude_high;
+	if(isset($_GET["distance_range"]) && $_GET["distance_range"] != '' )
+	{
+		$distanceFilter = isset($_GET["distance_range"]) ? $_GET["distance_range"] : 5;
+		$longitude_low = $long - ($distanceFilter / 100);
+		$longitude_high = $long + ($distanceFilter / 100);
+		$latitude_low = $lat - ($distanceFilter / 100);
+		$latitude_high = $lat + ($distanceFilter / 100);
+		$where = " and req.longitude between " . $longitude_low . " and " . $longitude_high . " and req.latitude between " . $latitude_low . " and " . $latitude_high;
+	}
+    
 	
 	// Filtering logic
     if (isset($_GET["service_type"]) && $_GET["service_type"] != '') {
@@ -124,7 +264,7 @@ function run_query() {
         array("screen_name" => "1Km", "id" => 1),
         array("screen_name" => "5Km", "id" => 5),
         array("screen_name" => "10Km", "id" => 10),
-        array("screen_name" => "15Km", "id" => 15)
+        array("screen_name" => "20Km", "id" => 20)
     );
     $data['duration_range'] = array(
         array("screen_name" => "1Hr", "id" => 1),
@@ -138,10 +278,11 @@ function run_query() {
     //
     mysqli_close($conn);
     return $data;
+	//return $sql_final;
 }
 
-$possible_url = array("get_app_list", "get_app", "get_locations", "filter_by");
-
+$possible_url = array("get_app_list", "get_app", "get_locations", "filter_by","volunteer_requests","get_requests");
+date_default_timezone_set('Asia/Kolkata');
 $value = "An error has occurred";
 $_empty_call = true;
 
@@ -169,6 +310,15 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) {
                 $value = get_location_autofill($_GET["loc"]);
             else
                 $value = "Missing argument";
+            break;
+		case "volunteer_requests":
+            if (isset($_GET["vid"]))
+                $value = get_volunteer_requests();
+            else
+                $value = "Missing argument";
+            break;
+		case "get_requests":
+            $value = get_all_requests();
             break;
     }
 }
