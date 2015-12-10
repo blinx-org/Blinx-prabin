2a38ef616666eb1c09cf79a7b76e0957b85ba16e
mraju2 <search4gyan@gmail.com>
2015-12-10 22:10:39 +0530

    acceptance
    New functions update request and create request functions added.

--- libs/acceptance.php
+++ libs/acceptance.php
@@ -2,13 +2,107 @@
 
 // This is the API, 2 possibilities: show the app list or show a specific app by id.
 // This would normally be pulled from a database but for demo purposes, I will be hardcoding the return values.
-
-function update_request() {
-    $status = $_POST["status"];
-    $vid = $_POST["vid"];
-    $helpid = $_POST["helpid"];
-    $query = "UPDATE t_help_request set status = '$status', volunteer_id = $vid where Id = $helpid ";
-    return update_query($query);
+date_default_timezone_set('Asia/Kolkata');
+function update_Accept_request() {
+    $date = date('Y-m-d H:i:s');
+    $data['Status'] = array(
+        array("DBStatus" => "2", "Message" => "Values are Empty")
+            );
+    $DbStatus=$data['Status'];
+    $status = isset($_POST["status"]) ? $_POST["status"] : '';
+    $vid = isset($_POST["vid"]) ? $_POST["vid"] : '';
+    $reqId = isset($_POST["reqID"]) ? $_POST["reqID"] : '';
+    $UsrId = isset($_POST["usrID"]) ? $_POST["usrID"] : '0';
+    $mId='0';
+    if($reqId=='' || $vid=='' || $status =='' )
+    {
+        echo json_encode($data['Status']);;
+    }
+    else
+    {
+        $mId=isset($_POST["Mid"]) ? $_POST["Mid"] : '0';
+        $query = "UPDATE t_help_request set Status = '$status', VolunteerId = $vid where Id = $reqId ";
+        $query_log="INSERT INTO t_help_request_log(Id,UserId ,Datetime,Status,VolunteerId,Mid) ".
+            " VALUES ( ".$reqId .",".$UsrId.",'".$date."','".$status."',".$vid.",".$mId.")";
+        include './dbconnection.php';
+    
+    $conn = mysqli_connect($host, $user, $pass, $database) or die("Error " . mysqli_error($link));
+    mysqli_begin_transaction($conn, MYSQLI_TRANS_START_READ_WRITE);
+    $value = mysqli_query($conn, $query);
+    $result=mysqli_query($conn, $query_log);
+        if($value && $result )
+        {
+            mysqli_commit($conn);
+            foreach($DbStatus as $key=>$bal) {
+                    $DbStatus[$key]['DBStatus']=$value;
+                    $DbStatus[$key]['Message']="Success";
+              }
+        }
+        else
+        {
+            mysqli_rollback($conn);
+        }
+        mysqli_close($conn);
+        echo json_encode($DbStatus);
+        //return $data['Status'];
+    }
+}
+function create_request() {
+    $data['Status'] = array(
+        array("DBStatus" => "2", "Message" => "Values are Empty")
+            );
+    $date = date('Y-m-d H:i:s');
+    $Uid = isset($_POST["Uid"]) ? $_POST["Uid"] : '';
+    $email = isset($_POST["email"])?$_POST["email"] : '';
+    $phone = isset($_POST["phone"])?$_POST["phone"] : '';
+    $helpdId = isset($_POST["helpId"])?$_POST["helpId"] : '';
+    $requestdate = isset($_POST["requesteddate"])?$_POST["requesteddate"] : '';
+    $place1= isset($_POST["location1"])?$_POST["location1"] : '';
+    $place2= isset($_POST["location2"])?$_POST["location2"] : '';
+    $location=$place1.$place2;
+    $message= isset($_POST["message"])?$_POST["message"] : '';
+    $address= isset($_POST["address"])?$_POST["address"] : '';
+    $education = isset($_POST["education"])?$_POST["education"] : '';
+    $latitide = isset($_POST["lat"])?$_POST["lat"] : '';
+    $logitude = isset($_POST["long"])?$_POST["long"] : '';
+    $duration = isset($_POST["duration"])?$_POST["duration"] : '';
+    $status = isset($_POST["status"]) ? $_POST["status"] : '';
+    $lang = isset($_POST["lang"]) ? $_POST["lang"] : '';
+    $meetingType = isset($_POST["meeting"]) ? $_POST["meeting"] : '';
+    
+    if($Uid=='' || $email=='' || $phone ==''||$helpdId==''||$latitide==''||$logitude=='' )
+    {
+        echo json_encode($data['Status']);;
+    }
+    else
+    {
+        $query = "select COALESCE(MAX(Id), 0) as MaxID from t_help_request";
+        $value=  update_query($query);
+        $row= mysqli_fetch_array( $value,MYSQLI_ASSOC);
+        $txnid=intval($row["MaxID"])+1;
+        $sql="INSERT INTO t_help_request (Id,phone,email,UserId,helpId,Message,Address,"
+                                . "Location,Requesteddate,Createddate,latitude,longitude,Duration)"
+                                . "VALUES( $txnid,'$phone','$email',$Uid,$helpdId,"
+                                . "'$message','$address','$place','$requestdate',"
+                                . "'$current_date','$latitide','$logitude','$duration')";
+        
+        
+        if($result)
+        {
+            mysqli_commit($conn);
+            foreach($DbStatus as $key=>$bal) {
+                    $DbStatus[$key]['DBStatus']=$value;
+                    $DbStatus[$key]['Message']="Success";
+              }
+        }
+        else
+        {
+            mysqli_rollback($conn);
+        }
+        mysqli_close($conn);
+        echo json_encode($DbStatus);
+        //return $data['Status'];
+    }
 }
 
 function insert_ratings() {
@@ -18,8 +112,9 @@ function insert_ratings() {
     $comments = $_POST["comments"];
     $rating = $_POST["rating"];
     $query = "INSERT  into ratings (user_id,help_request_id,comments,rating) values ($user_id,$help_request_id,'$comments', $rating) ";
-
-    return update_query($query);
+    
+    return $query;
+    //return update_query($query);
 }
 
 function update_query($query) {
@@ -28,7 +123,7 @@ function update_query($query) {
     $conn = mysqli_connect($host, $user, $pass, $database) or die("Error " . mysqli_error($link));
 
 
-    echo $query;
+    //echo $query;
 
     $result = mysqli_query($conn, $query);
     if (!$result) {
@@ -36,7 +131,7 @@ function update_query($query) {
         exit;
     }
     mysqli_close($conn);
-    return $query;
+    return $result;
 }
 
 $possible_url = array("update_request", "put_ratings");
@@ -47,10 +142,13 @@ $value = "An error has occurred";
 if (isset($_POST["action"]) && in_array($_POST["action"], $possible_url)) {
     switch ($_POST["action"]) {
         case "update_request":
-            update_request();
+            update_Accept_request();
             break;
         case "put_ratings" :
             insert_ratings();
+            break;
+        case "create_request":
+            create_request();
             break;
     }
 }
