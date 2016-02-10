24f5241b1791c4c96f57fbd23ef06cfd42753182
mraju2 <search4gyan@gmail.com>
2016-02-10 16:45:54 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -16,7 +16,7 @@ function mailto($mail,$id)
     };
     if(!$value1)
         include '.\libs\dbconnection.php';
-    
+    //Join
     if($mail=="1")
     {
         $dbHelper=new DB();
@@ -38,8 +38,7 @@ function mailto($mail,$id)
                 "<br></br>".
             "Firstly, a heartfelt 'Thank you' for having registered on BLINX a Volunteer..".
             "It means you believe in the power of good, in our country's '".
-            "future and your ability to make a change. Yet, since you haven't made any contributions yet, '".
-            "I'm assuming something is holding you back." .
+            "future and your ability to make a change.".
             "I look forward to hearing from you! </P>'";
         $message.='<div style=font-family: Arial;><br/>';
         $message.='click on the below link to verify your account ';
@@ -48,6 +47,7 @@ function mailto($mail,$id)
         $message.='</body></html>';
         mail($to, $subject, $message, $headers);
     }
+	//RequestAcceptance
     if($mail=="2")
     {
         $value=false;
@@ -81,6 +81,219 @@ function mailto($mail,$id)
         $dbHelper=new DB();
         $data=run_query($id);
         session_start();
+        //$vid=$_SESSION['mobile'];
+        $vid='9538088668';
+		//$uid=$data['uid'];
+		$uid=1;
+        $query = "SELECT * FROM  m_volunteer v where v.mobile_number = '".$vid."'";
+        $resultVol=$dbHelper->runSelectQuery($query);
+		$query = "SELECT * FROM  m_user u where u.user_id = '".$uid."'";
+        $resultUsr=$dbHelper->runSelectQuery($query);
+        $to= $resultVol[0]['email_id'];
+        $vid= $resultVol[0]['volunteer_id'];
+        $subject = 'Request Acceptance';
+        $headers = "From: blinx.app@gmail.com \r\n";
+        $headers .= "Reply-To: email@domain.com \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $message = '<html><body>';
+        $message.='<p>Dear '. $resultVol[0]['first_name'] . $resultVol[0]['last_name']." <br></br>".
+            "Thanks you for accepting the request. The below are request details.".
+            '</P>';
+        $message.='<h4> Name :'.$data['first_name'] ." ". $data['last_name'].'</h4>'.
+                    '<h4> Mobile :'. $data["mobile_number"]. '</h4>'.
+                    '<h4> Type of service :'. $data["Description"]. '</h4>'.
+					'<h4> Duration : '.$data["duration"].'Hrs </h4>'.
+                    '<h4> Requested Date :'.$data["Requesteddate"].'</h4>'.
+                    '<h4> Location : '.$data["Location"].'</h4>'.
+                    '<h4> Location : https://www.google.com/maps/place/'. $data["latitude"].','.$data["longitude"].'<h4>'.
+                    '<h4> Message : '.$data["Message"].'</h4>'.
+                    '<h4> Address : '.$data["Address"].'</h4>';
+        $message.='</body></html>';
+		$html=$message;
+		echo $html;
+        //mail($to, $subject, $message, $headers);
+		$message='';
+		$message = '<html><body>';
+        $message.='<p>Dear '. $resultUsr[0]['first_name'] . $resultUsr[0]['last_name']." <br></br>".
+            "Your Request is accepted. The below are Volunteer details.".
+            '</P>';
+		$message.='<h4> Name :'.$resultVol[0]['first_name'] ." ". $resultVol['last_name'].'</h4>'.
+                    '<h4> Mobile :'. $resultVol["mobile_number"]. '</h4>'.
+                    '<h4> E-mail :'. $resultVol["email_id"]. '</h4>'.
+					'<h4> Address : '.$resultVol["address"].'Hrs </h4>';
+		$message.="<br></br>".
+            "The below are Request details.".
+            '</P>';
+        $message.='<h4> Name :'.$data['first_name'] ." ". $data['last_name'].'</h4>'.
+                    '<h4> Mobile :'. $data["mobile_number"]. '</h4>'.
+                    '<h4> Type of service :'. $data["Description"]. '</h4>'.
+					'<h4> Duration : '.$data["duration"].'Hrs </h4>'.
+                    '<h4> Requested Date :'.$data["Requesteddate"].'</h4>'.
+                    '<h4> Location : '.$data["Location"].'</h4>'.
+                    '<h4> Location : https://www.google.com/maps/place/'. $data["latitude"].','.$data["longitude"].'<h4>'.
+                    '<h4> Message : '.$data["Message"].'</h4>'.
+                    '<h4> Address : '.$data["Address"].'</h4>';
+		
+        $message.='</body></html>';
+		$html1=$message;
+		echo $html1;
+    }
+	//Request Cancelled.
+    if($mail=="3")
+    {
+        $value=false;
+        $value1=false;
+        $included_files=get_included_files();
+        foreach ($included_files as $filename) 
+        {
+            $pieces = explode("\\", $filename);
+            $value=in_array("dbconnection.php", $pieces);
+            if($value==true)
+            {
+                $value1=true;
+            };
+        };
+        if(!$value1)
+            include '.\libs\dbconnection.php';
+        $value=false;
+        $value1=false;
+        foreach ($included_files as $filename) 
+        {
+            $pieces = explode("\\", $filename);
+            $value=in_array("request.php", $pieces);
+            if($value==true)
+            {
+                $value1=true;
+            };
+        };
+        if(!$value1)
+            include '.\libs\request.php';
+   
+        $dbHelper=new DB();
+        $data=run_query($id);
+        session_start();
+        //$vid=$_SESSION['mobile'];
+        $vid='9538088668';
+		//$uid=$data['uid'];
+		$uid=1;
+        $query = "SELECT * FROM  m_volunteer v where v.mobile_number = '".$vid."'";
+        $resultVol=$dbHelper->runSelectQuery($query);
+		$query = "SELECT * FROM  m_user u where u.user_id = '".$uid."'";
+        $resultUsr=$dbHelper->runSelectQuery($query);
+        $to= $resultVol[0]['email_id'];
+        $vid= $resultVol[0]['volunteer_id'];
+        $subject = 'Request Acceptance';
+        $headers = "From: blinx.app@gmail.com \r\n";
+        $headers .= "Reply-To: email@domain.com \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $message = '<html><body>';
+        $message.='<p>Dear '. $resultVol[0]['first_name'] . $resultVol[0]['last_name']." <br></br>".
+            "The request is cancelled. The below are request details.".
+            '</P>';
+        $message.='<h4> Name :'.$data['first_name'] ." ". $data['last_name'].'</h4>'.
+                    '<h4> Mobile :'. $data["mobile_number"]. '</h4>'.
+                    '<h4> Type of service :'. $data["Description"]. '</h4>'.
+					'<h4> Duration : '.$data["duration"].'Hrs </h4>'.
+                    '<h4> Requested Date :'.$data["Requesteddate"].'</h4>'.
+                    '<h4> Location : '.$data["Location"].'</h4>'.
+                    '<h4> Location : https://www.google.com/maps/place/'. $data["latitude"].','.$data["longitude"].'<h4>'.
+                    '<h4> Message : '.$data["Message"].'</h4>'.
+                    '<h4> Address : '.$data["Address"].'</h4>';
+        $message.='</body></html>';
+		$html=$message;
+		echo $html;
+        //mail($to, $subject, $message, $headers);
+		$message='';
+		$message = '<html><body>';
+        $message.='<p>Dear '. $resultUsr[0]['first_name'] . $resultUsr[0]['last_name']." <br></br>".
+            "Your Request is cancelled. The below are Volunteer details cancelled the request.".
+            '</P>';
+		$message.='<h4> Name :'.$resultVol[0]['first_name'] ." ". $resultVol['last_name'].'</h4>'.
+                    '<h4> Mobile :'. $resultVol["mobile_number"]. '</h4>'.
+                    '<h4> E-mail :'. $resultVol["email_id"]. '</h4>'.
+					'<h4> Address : '.$resultVol["address"].'Hrs </h4>';
+		$message.="<br></br>".
+            "The below are Request details.".
+            '</P>';
+        $message.='<h4> Name :'.$data['first_name'] ." ". $data['last_name'].'</h4>'.
+                    '<h4> Mobile :'. $data["mobile_number"]. '</h4>'.
+                    '<h4> Type of service :'. $data["Description"]. '</h4>'.
+					'<h4> Duration : '.$data["duration"].'Hrs </h4>'.
+                    '<h4> Requested Date :'.$data["Requesteddate"].'</h4>'.
+                    '<h4> Location : '.$data["Location"].'</h4>'.
+                    '<h4> Location : https://www.google.com/maps/place/'. $data["latitude"].','.$data["longitude"].'<h4>'.
+                    '<h4> Message : '.$data["Message"].'</h4>'.
+                    '<h4> Address : '.$data["Address"].'</h4>';
+		
+        $message.='</body></html>';
+		$html1=$message;
+		echo $html1;
+    }
+	//Chnage Password
+	if($mail=="4")
+    {
+        $value=false;
+        $value1=false;
+        $included_files=get_included_files();
+        foreach ($included_files as $filename) 
+        {
+            $pieces = explode("\\", $filename);
+            $value=in_array("dbconnection.php", $pieces);
+            if($value==true)
+            {
+                $value1=true;
+            };
+        };
+        if(!$value1)
+            include '.\libs\dbconnection.php';
+   
+        $dbHelper=new DB();
+        session_start();
+        $vid=$_SESSION['mobile'];
+        $vid='9538088668';
+        $query = "SELECT * FROM  m_volunteer v where v.mobile_number = '".$vid."'";
+        $result=$dbHelper->runSelectQuery($query);
+        $to= $result[0]['email_id'];
+        $vid= $result[0]['volunteer_id'];
+        $confirmCode= $result[0]['confirmCode'];
+        $subject = 'Change Password';
+        $headers = "From: blinx.app@gmail.com \r\n";
+        $headers .= "Reply-To: email@domain.com \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $message = '<html><body>';
+        $message.='<div style="width:550px; background-color:#CC6600; padding:15px; font-weight:bold;">';
+        $message.='Request ACceptance';
+        $message.='</div>';
+        $message.='<p>Dear '. $result[0]['first_name'] . $result[0]['last_name']." <br></br>".
+            "Your password is changed.".
+            '</P>';
+        $message.='</body></html>';
+        mail($to, $subject, $message, $headers);
+    }
+	//ForgotPassword
+	if($mail=="4")
+    {
+        $value=false;
+        $value1=false;
+        $included_files=get_included_files();
+        foreach ($included_files as $filename) 
+        {
+            $pieces = explode("\\", $filename);
+            $value=in_array("dbconnection.php", $pieces);
+            if($value==true)
+            {
+                $value1=true;
+            };
+        };
+        if(!$value1)
+            include '.\libs\dbconnection.php';
+  
+        $dbHelper=new DB();
+        $data=run_query($id);
+        session_start();
         $vid=$_SESSION['mobile'];
         $vid='9538088668';
         $query = "SELECT * FROM  m_volunteer v where v.mobile_number = '".$vid."'";
@@ -99,24 +312,13 @@ function mailto($mail,$id)
         $message.='</div>';
         $message.='<p>Dear '. $result[0]['first_name'] . $result[0]['last_name']." <br></br>".
                 "<br></br>".
-            "Thanks you for accepting the request. The below are request details.".
+            "your request is cancelled. The below are request details.".
             '</P>';
-        $message.='<h4> Name :'.$data['first_name'] ." ". $data['last_name'].'</h4>'.
-                    '<h4> Mobile :'. $data["mobile_number"]. '</h4>'.
-                    '<h4> Type of service :'. $data["Description"]. '</h4>'.
-		    '<h4> Duration : '.$data["duration"].'Hrs </h4>'.
-                    '<h4> Requested Date :'.$data["Requesteddate"].'</h4>'.
-                    '<h4> Location : '.$data["Location"].'</h4>'.
-                    '<h4> Location : https://www.google.com/maps/place/'. $data["latitude"].','.$data["longitude"].'<h4>'.
-                    '<h4> Message : '.$data["Message"].'</h4>'.
-                    '<h4> Address : '.$data["Address"].'</h4>'.
-                    '</p>';
-        $message.='<div style=font-family: Arial;><br/>';
-        $message.='</div>';
         $message.='</body></html>';
         mail($to, $subject, $message, $headers);
     }
-    if($mail=="3")
+	//ContactUs
+	if($mail=="4")
     {
         $value=false;
         $value1=false;
@@ -184,25 +386,6 @@ function mailto($mail,$id)
         $message.='</body></html>';
         mail($to, $subject, $message, $headers);
     }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-            }
+}
 
 ?>
