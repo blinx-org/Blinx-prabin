4bea740f293adc4d9fff3c2e4ea5bfb2c3f21eae
mraju2 <search4gyan@gmail.com>
2016-02-11 06:20:24 +0530

    Mail Changes and Search function added

--- tags/search/search-header.php
+++ tags/search/search-header.php
@@ -1,13 +1,14 @@
 <div class="row">
     <div class="container">
-        <div class="col-sm-12 sr-tagline">
+        <div class="col-md-1 sr-tagline"></div>
+        <div class="col-md-10 sr-tagline">
             <div class="input-group">
-                <form id="frmSearch" class="form-inline" action="<?php echo URL_SEARCH ?>" method="GET">
+                <form id="frmSearch" text-align="center" class="form-inline" action="<?php echo URL_SEARCH ?>" method="GET">
                     <select class="" style="margin: 0;
     display: block;
     padding: 6px 4px;
     border: 1px solid #ccc;
-    width: 20%;
+ 
     -moz-box-sizing: content-box;
     -webkit-box-sizing:content-box;
     box-sizing:content-box;
@@ -15,14 +16,14 @@
     line-height: 25px;
     font-size: 12px;
     float: left;"  id="helpType" onchange="onOptionChange();">
-                        <option value="None" selected>HelpByMeetingPersonally</option>
-                        <option value="DeSoto">HelpOverInternet</option>
+                        <option value="None" selected>Help By Meeting Personally</option>
+                        <option value="DeSoto">Help Over Internet</option>
                     </select>
                     <input class="" id="my-address" style="margin: 0;
     display: block;
     padding: 6px 4px;
+    width:500px;
     border: 1px solid #ccc;
-    width: 70%;
     -moz-box-sizing: content-box;
     -webkit-box-sizing:content-box;
     box-sizing:content-box;
@@ -30,18 +31,26 @@
     line-height: 15px;
     font-size: 12px;
     float: left;"autocomplete="off" spellcheck="false" placeholder="I'm looking around..." >
+                    <button class="btn btn-primary" style="margin: 0;
+    display: block;
+    -moz-box-sizing: content-box;
+    -webkit-box-sizing:content-box;
+    box-sizing:content-box;
+    width:100px;
+    height: 30px;
+    line-height: 15px;
+    font-size: 12px;
+    float: next;" type="submit" value="search"  onClick="doSearch();" >search
+                    </button>
                     <input type="hidden" name="long" value="" />
                     <input type="hidden" name="lat" value="" />
                     <input type="hidden" name="action" id="action" value="get_app" />
                 </form>
                 <span class="input-group-btn">
-                    <button class="btn btn-primary" type="submit" value="search" onClick="doSearch();" >
-                        <b>
-                            <span class="offshowtext">search</span>
-                        </b>
-                    </button>
+                    
                 </span>
             </div>
         </div>
+        <div class="col-sm-1 sr-tagline"></div>
     </div>
 </div>
